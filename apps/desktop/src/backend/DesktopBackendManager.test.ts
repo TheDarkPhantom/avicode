nico2525nn/t3code

Buffer backend output before persisting failure logs
- Capture child process output in memory during normal runs
- Persist buffered output only on failures and bound retained data
- Increase trace/event batching windows to reduce flush churn

apps/desktop/src/backend/DesktopBackendManager.test.ts
@@ -140,8 +140,10 @@ function makeManagerLayer(input: {
           ? Layer.succeed(DesktopState.DesktopState, input.desktopState)
           : DesktopState.layer,
         Layer.succeed(DesktopObservability.DesktopBackendOutputLog, {
-          writeSessionBoundary: () => Effect.void,
+          beginSession: () => Effect.void,
           writeOutputChunk: () => Effect.void,
+          persistFailure: () => Effect.void,
+          discardSession: Effect.void,
           ...input.backendOutputLog,
         } satisfies DesktopObservability.DesktopBackendOutputLogShape),
         Layer.succeed(DesktopWindow.DesktopWindow, {
@@ -200,8 +202,7 @@ describe("DesktopBackendManager", () => {
           }).pipe(Effect.andThen(Deferred.succeed(ready, void 0))),
         },
         backendOutputLog: {
-          writeSessionBoundary: ({ phase }) =>
-            phase === "END" ? Queue.offer(exited, void 0).pipe(Effect.asVoid) : Effect.void,
+          persistFailure: () => Queue.offer(exited, void 0).pipe(Effect.asVoid),
         },
       });
 
@@ -272,6 +273,52 @@ describe("DesktopBackendManager", () => {
     }),
   );
 
+  it.effect("drains trailing child output before persisting an unexpected exit", () =>
+    Effect.gen(function* () {
+      const persistedOutput = yield* Deferred.make<ReadonlyArray<string>>();
+      const outputDrainStarted = yield* Deferred.make<void>();
+      const outputChunks = yield* Ref.make<Array<string>>([]);
+      const spawnerLayer = Layer.succeed(
+        ChildProcessSpawner.ChildProcessSpawner,
+        ChildProcessSpawner.make(() =>
+          Effect.succeed(
+            makeProcess({
+              stdout: Stream.fromEffect(
+                Deferred.succeed(outputDrainStarted, void 0).pipe(
+                  Effect.andThen(Effect.sleep(Duration.millis(50))),
+                  Effect.as(new TextEncoder().encode("trailing output\n")),
+                ),
+              ),
+              exitCode: Effect.succeed(ChildProcessSpawner.ExitCode(1)),
+            }),
+          ),
+        ),
+      );
+      const managerLayer = makeManagerLayer({
+        spawnerLayer,
+        httpClientLayer: httpClientLayer(() => Effect.never),
+        backendOutputLog: {
+          writeOutputChunk: (_streamName, chunk) =>
+            Ref.update(outputChunks, (current) => [...current, new TextDecoder().decode(chunk)]),
+          persistFailure: () =>
+            Ref.get(outputChunks).pipe(
+              Effect.flatMap((chunks) => Deferred.succeed(persistedOutput, chunks)),
+              Effect.asVoid,
+            ),
+        },
+      });
+
+      yield* Effect.gen(function* () {
+        const manager = yield* DesktopBackendManager.DesktopBackendManager;
+        yield* manager.start;
+        yield* Deferred.await(outputDrainStarted);
+        yield* TestClock.adjust(Duration.millis(50));
+
+        assert.deepEqual(yield* Deferred.await(persistedOutput), ["trailing output\n"]);
+      }).pipe(Effect.provide(Layer.merge(TestClock.layer(), managerLayer)));
+    }),
+  );
+
   it.effect("retries HTTP readiness before reporting the backend ready", () =>
     Effect.gen(function* () {
       const requestUrls: Array<string> = [];
@@ -309,8 +356,7 @@ describe("DesktopBackendManager", () => {
           }).pipe(Effect.andThen(Deferred.succeed(ready, void 0))),
         },
         backendOutputLog: {
-          writeSessionBoundary: ({ phase }) =>
-            phase === "END" ? Queue.offer(exited, void 0).pipe(Effect.asVoid) : Effect.void,
+          persistFailure: () => Queue.offer(exited, void 0).pipe(Effect.asVoid),
         },
       });
 
@@ -343,6 +389,8 @@ describe("DesktopBackendManager", () => {
       const ready = yield* Deferred.make<void>();
       const backendReady = yield* Ref.make(false);
       const quitting = yield* Ref.make(false);
+      let persistedFailureCount = 0;
+      let discardedSessionCount = 0;
 
       const spawnerLayer = Layer.succeed(
         ChildProcessSpawner.ChildProcessSpawner,
@@ -374,6 +422,15 @@ describe("DesktopBackendManager", () => {
         desktopWindow: {
           handleBackendReady: Deferred.succeed(ready, void 0).pipe(Effect.asVoid),
         },
+        backendOutputLog: {
+          persistFailure: () =>
+            Effect.sync(() => {
+              persistedFailureCount += 1;
+            }),
+          discardSession: Effect.sync(() => {
+            discardedSessionCount += 1;
+          }),
+        },
       });
 
       yield* Effect.gen(function* () {
@@ -393,6 +450,8 @@ describe("DesktopBackendManager", () => {
         yield* manager.stop();
         assert.equal(startCount, 1);
         assert.equal(closedCount, 1);
+        assert.equal(persistedFailureCount, 0);
+        assert.equal(discardedSessionCount, 1);
 
         const stoppedSnapshot = yield* manager.snapshot;
         assert.isFalse(yield* Ref.get(backendReady));
@@ -406,6 +465,7 @@ describe("DesktopBackendManager", () => {
   it.effect("restarts an unexpectedly exited backend with the Effect clock", () =>
     Effect.gen(function* () {
       const starts = yield* Queue.unbounded<number>();
+      const failures = yield* Queue.unbounded<string>();
       let startCount = 0;
 
       const spawnerLayer = Layer.succeed(
@@ -425,6 +485,9 @@ describe("DesktopBackendManager", () => {
       const managerLayer = makeManagerLayer({
         spawnerLayer,
         httpClientLayer: httpClientLayer(() => Effect.never),
+        backendOutputLog: {
+          persistFailure: ({ details }) => Queue.offer(failures, details).pipe(Effect.asVoid),
+        },
       });
 
       yield* Effect.gen(function* () {
@@ -432,6 +495,7 @@ describe("DesktopBackendManager", () => {
         yield* manager.start;
 
         assert.equal(yield* Queue.take(starts), 1);
+        assert.equal(yield* Queue.take(failures), "pid=123 code=1");
 
         yield* TestClock.adjust(Duration.millis(499));
         assert.equal(yield* Queue.size(starts), 0);
@@ -488,6 +552,13 @@ describe("DesktopBackendManager", () => {
         yield* manager.start;
 
         assert.equal(yield* Queue.take(starts), 1);
+        let restartScheduled = false;
+        while (!restartScheduled) {
+          restartScheduled = (yield* manager.snapshot).restartScheduled;
+          if (!restartScheduled) {
+            yield* Effect.yieldNow;
+          }
+        }
 
         yield* manager.start;
         assert.equal(yield* Queue.take(starts), 2);

apps/desktop/src/backend/DesktopBackendManager.ts
@@ -39,6 +39,7 @@ const DEFAULT_BACKEND_READINESS_TIMEOUT = Duration.minutes(1);
 const DEFAULT_BACKEND_READINESS_INTERVAL = Duration.millis(100);
 const DEFAULT_BACKEND_READINESS_REQUEST_TIMEOUT = Duration.seconds(1);
 const DEFAULT_BACKEND_TERMINATE_GRACE = Duration.seconds(2);
+const DEFAULT_BACKEND_OUTPUT_DRAIN_TIMEOUT = Duration.millis(250);
 const BACKEND_READINESS_PATH = "/.well-known/t3/environment";
 
 type BackendProcessLayerServices = ChildProcessSpawner.ChildProcessSpawner | HttpClient.HttpClient;
@@ -280,6 +281,7 @@ const runBackendProcess = Effect.fn("runBackendProcess")(function* (
   const handle = yield* spawner
     .spawn(command)
     .pipe(Effect.mapError((cause) => new BackendProcessSpawnError({ cause })));
+  const outputFibers: Array<Fiber.Fiber<void, never>> = [];
 
   yield* options.onStarted?.(handle.pid) ?? Effect.void;
   if (
@@ -309,8 +311,10 @@ const runBackendProcess = Effect.fn("runBackendProcess")(function* (
     );
   }
   if (options.captureOutput) {
-    yield* drainBackendOutput("stdout", handle.stdout, onOutput).pipe(Effect.forkScoped);
-    yield* drainBackendOutput("stderr", handle.stderr, onOutput).pipe(Effect.forkScoped);
+    outputFibers.push(
+      yield* drainBackendOutput("stdout", handle.stdout, onOutput).pipe(Effect.forkScoped),
+      yield* drainBackendOutput("stderr", handle.stderr, onOutput).pipe(Effect.forkScoped),
+    );
   }
   yield* waitForHttpReady(
     options.httpBaseUrl,
@@ -321,7 +325,12 @@ const runBackendProcess = Effect.fn("runBackendProcess")(function* (
     Effect.forkScoped,
   );
 
-  return describeProcessExit(yield* Effect.result(handle.exitCode));
+  const exit = describeProcessExit(yield* Effect.result(handle.exitCode));
+  yield* Effect.forEach(outputFibers, Fiber.await, {
+    concurrency: "unbounded",
+    discard: true,
+  }).pipe(Effect.timeout(DEFAULT_BACKEND_OUTPUT_DRAIN_TIMEOUT), Effect.ignore);
+  return exit;
 });
 
 const makeDesktopBackendManager = Effect.fn("makeDesktopBackendManager")(function* () {
@@ -467,10 +476,13 @@ const makeDesktopBackendManager = Effect.fn("makeDesktopBackendManager")(functio
 
               if (isCurrentRun) {
                 if (Option.isSome(pid)) {
-                  yield* backendOutputLog.writeSessionBoundary({
-                    phase: "END",
-                    details: `pid=${pid.value} ${reason}`,
-                  });
+                  if (nextState.desiredRunning) {
+                    yield* backendOutputLog.persistFailure({
+                      details: `pid=${pid.value} ${reason}`,
+                    });
+                  } else {
+                    yield* backendOutputLog.discardSession;
+                  }
                 }
                 yield* Ref.set(desktopState.backendReady, false);
               }
@@ -491,8 +503,7 @@ const makeDesktopBackendManager = Effect.fn("makeDesktopBackendManager")(functio
               ...run,
               pid: Option.some(pid),
             }));
-            yield* backendOutputLog.writeSessionBoundary({
-              phase: "START",
+            yield* backendOutputLog.beginSession({
               details: `pid=${pid} port=${config.value.bootstrap.port} cwd=${config.value.cwd}`,
             });
           }),
@@ -525,10 +536,16 @@ const makeDesktopBackendManager = Effect.fn("makeDesktopBackendManager")(functio
               ),
             );
           }),
-          onReadinessFailure: (error) =>
-            logBackendManagerWarning("backend readiness check failed during bootstrap", {
-              error: error.message,
-            }),
+          onReadinessFailure: Effect.fn("desktop.backendManager.onReadinessFailure")(
+            function* (error) {
+              yield* logBackendManagerWarning("backend readiness check failed during bootstrap", {
+                error: error.message,
+              });
+              yield* backendOutputLog.persistFailure({
+                details: error.message,
+              });
+            },
+          ),
           onOutput: (streamName, chunk) => backendOutputLog.writeOutputChunk(streamName, chunk),
         }).pipe(
           Effect.provideService(ChildProcessSpawner.ChildProcessSpawner, spawner),
@@ -639,7 +656,7 @@ const makeDesktopBackendManager = Effect.fn("makeDesktopBackendManager")(functio
     });
     yield* Option.match(active, {
       onNone: () => Effect.void,
-      onSome: (run) => closeRun(run, options),
+      onSome: (run) => closeRun(run, options).pipe(Effect.andThen(backendOutputLog.discardSession)),
     });
   });
 

TheDarkPhantom/avicode

Co-authored-by: codex <codex@users.noreply.github.com>

apps/desktop/src/backend/DesktopBackendManager.test.ts
@@ -135,8 +135,10 @@ interface MakeInstanceInput {
 // to drive the instance's lifecycle.
 function makeTestInstance(input: MakeInstanceInput) {
   const stubLog: DesktopObservability.DesktopBackendOutputLogShape = {
-    writeSessionBoundary: () => Effect.void,
+    beginSession: () => Effect.void,
     writeOutputChunk: () => Effect.void,
+    persistFailure: () => Effect.void,
+    discardSession: Effect.void,
     ...input.backendOutputLog,
   };
   const servicesLayer = Layer.mergeAll(
@@ -216,8 +218,7 @@ describe("DesktopBackendManager", () => {
             readyCount += 1;
           }).pipe(Effect.andThen(Deferred.succeed(ready, void 0)), Effect.asVoid),
           backendOutputLog: {
-            writeSessionBoundary: ({ phase }) =>
-              phase === "END" ? Queue.offer(exited, void 0).pipe(Effect.asVoid) : Effect.void,
+            persistFailure: () => Queue.offer(exited, void 0).pipe(Effect.asVoid),
           },
         });
 
@@ -289,6 +290,51 @@ describe("DesktopBackendManager", () => {
     ),
   );
 
+  it.effect("drains trailing child output before persisting an unexpected exit", () =>
+    Effect.scoped(
+      Effect.gen(function* () {
+        const persistedOutput = yield* Deferred.make<ReadonlyArray<string>>();
+        const outputDrainStarted = yield* Deferred.make<void>();
+        const outputChunks = yield* Ref.make<Array<string>>([]);
+        const spawnerLayer = Layer.succeed(
+          ChildProcessSpawner.ChildProcessSpawner,
+          ChildProcessSpawner.make(() =>
+            Effect.succeed(
+              makeProcess({
+                stdout: Stream.fromEffect(
+                  Deferred.succeed(outputDrainStarted, void 0).pipe(
+                    Effect.andThen(Effect.sleep(Duration.millis(50))),
+                    Effect.as(new TextEncoder().encode("trailing output\n")),
+                  ),
+                ),
+                exitCode: Effect.succeed(ChildProcessSpawner.ExitCode(1)),
+              }),
+            ),
+          ),
+        );
+        const instance = yield* makeTestInstance({
+          spawnerLayer,
+          httpClientLayer: httpClientLayer(() => Effect.never),
+          backendOutputLog: {
+            writeOutputChunk: (_streamName, chunk) =>
+              Ref.update(outputChunks, (current) => [...current, new TextDecoder().decode(chunk)]),
+            persistFailure: () =>
+              Ref.get(outputChunks).pipe(
+                Effect.flatMap((chunks) => Deferred.succeed(persistedOutput, chunks)),
+                Effect.asVoid,
+              ),
+          },
+        });
+
+        yield* instance.start;
+        yield* Deferred.await(outputDrainStarted);
+        yield* TestClock.adjust(Duration.millis(50));
+
+        assert.deepEqual(yield* Deferred.await(persistedOutput), ["trailing output\n"]);
+      }).pipe(Effect.provide(TestClock.layer())),
+    ),
+  );
+
   it.effect("retries HTTP readiness before reporting the backend ready", () =>
     Effect.scoped(
       Effect.gen(function* () {
@@ -325,8 +371,7 @@ describe("DesktopBackendManager", () => {
             readyCount += 1;
           }).pipe(Effect.andThen(Deferred.succeed(ready, void 0)), Effect.asVoid),
           backendOutputLog: {
-            writeSessionBoundary: ({ phase }) =>
-              phase === "END" ? Queue.offer(exited, void 0).pipe(Effect.asVoid) : Effect.void,
+            persistFailure: () => Queue.offer(exited, void 0).pipe(Effect.asVoid),
           },
         });
 
@@ -357,6 +402,8 @@ describe("DesktopBackendManager", () => {
         const startedPids = yield* Queue.unbounded<number>();
         const ready = yield* Deferred.make<void>();
         const backendReadyFlag = yield* Ref.make(false);
+        let persistedFailureCount = 0;
+        let discardedSessionCount = 0;
 
         const spawnerLayer = Layer.succeed(
           ChildProcessSpawner.ChildProcessSpawner,
@@ -386,6 +433,15 @@ describe("DesktopBackendManager", () => {
             Effect.asVoid,
           ),
           onShutdown: Ref.set(backendReadyFlag, false),
+          backendOutputLog: {
+            persistFailure: () =>
+              Effect.sync(() => {
+                persistedFailureCount += 1;
+              }),
+            discardSession: Effect.sync(() => {
+              discardedSessionCount += 1;
+            }),
+          },
         });
         assert.isTrue(Option.isNone(yield* instance.currentConfig));
 
@@ -402,6 +458,8 @@ describe("DesktopBackendManager", () => {
         yield* instance.stop();
         assert.equal(startCount, 1);
         assert.equal(closedCount, 1);
+        assert.equal(persistedFailureCount, 0);
+        assert.equal(discardedSessionCount, 1);
 
         const stoppedSnapshot = yield* instance.snapshot;
         assert.isFalse(yield* Ref.get(backendReadyFlag));
@@ -452,6 +510,7 @@ describe("DesktopBackendManager", () => {
     Effect.scoped(
       Effect.gen(function* () {
         const starts = yield* Queue.unbounded<number>();
+        const failures = yield* Queue.unbounded<string>();
         let startCount = 0;
 
         const spawnerLayer = Layer.succeed(
@@ -471,11 +530,15 @@ describe("DesktopBackendManager", () => {
         const instance = yield* makeTestInstance({
           spawnerLayer,
           httpClientLayer: httpClientLayer(() => Effect.never),
+          backendOutputLog: {
+            persistFailure: ({ details }) => Queue.offer(failures, details).pipe(Effect.asVoid),
+          },
         });
 
         yield* instance.start;
 
         assert.equal(yield* Queue.take(starts), 1);
+        assert.equal(yield* Queue.take(failures), "pid=123 code=1");
 
         yield* TestClock.adjust(Duration.millis(499));
         assert.equal(yield* Queue.size(starts), 0);
@@ -726,6 +789,13 @@ describe("DesktopBackendManager", () => {
         yield* instance.start;
 
         assert.equal(yield* Queue.take(starts), 1);
+        let restartScheduled = false;
+        while (!restartScheduled) {
+          restartScheduled = (yield* instance.snapshot).restartScheduled;
+          if (!restartScheduled) {
+            yield* Effect.yieldNow;
+          }
+        }
 
         yield* instance.start;
         assert.equal(yield* Queue.take(starts), 2);

apps/desktop/src/backend/DesktopBackendManager.ts
@@ -66,6 +66,7 @@ const DEFAULT_BACKEND_READINESS_TIMEOUT = Duration.minutes(1);
 const DEFAULT_BACKEND_READINESS_INTERVAL = Duration.millis(100);
 const DEFAULT_BACKEND_READINESS_REQUEST_TIMEOUT = Duration.seconds(1);
 const DEFAULT_BACKEND_TERMINATE_GRACE = Duration.seconds(2);
+const DEFAULT_BACKEND_OUTPUT_DRAIN_TIMEOUT = Duration.millis(250);
 const BACKEND_READINESS_PATH = "/.well-known/t3/environment";
 const { logWarning: logBackendProcessWarning } =
   DesktopObservability.makeComponentLogger("desktop-backend-process");
@@ -389,6 +390,7 @@ const runBackendProcess = Effect.fn("runBackendProcess")(function* (
         (cause) => new BackendProcessSpawnError({ executablePath: options.executablePath, cause }),
       ),
     );
+  const outputFibers: Array<Fiber.Fiber<void, never>> = [];
 
   yield* options.onStarted?.(handle.pid) ?? Effect.void;
   if (
@@ -418,8 +420,10 @@ const runBackendProcess = Effect.fn("runBackendProcess")(function* (
     );
   }
   if (options.captureOutput) {
-    yield* drainBackendOutput("stdout", handle.stdout, onOutput).pipe(Effect.forkScoped);
-    yield* drainBackendOutput("stderr", handle.stderr, onOutput).pipe(Effect.forkScoped);
+    outputFibers.push(
+      yield* drainBackendOutput("stdout", handle.stdout, onOutput).pipe(Effect.forkScoped),
+      yield* drainBackendOutput("stderr", handle.stderr, onOutput).pipe(Effect.forkScoped),
+    );
   }
   yield* waitForHttpReady(
     options.httpBaseUrl,
@@ -430,7 +434,12 @@ const runBackendProcess = Effect.fn("runBackendProcess")(function* (
     Effect.forkScoped,
   );
 
-  return describeProcessExit(yield* Effect.result(handle.exitCode));
+  const exit = yield* Effect.result(handle.exitCode);
+  yield* Effect.forEach(outputFibers, Fiber.await, {
+    concurrency: "unbounded",
+    discard: true,
+  }).pipe(Effect.timeout(DEFAULT_BACKEND_OUTPUT_DRAIN_TIMEOUT), Effect.ignore);
+  return describeProcessExit(exit);
 });
 
 // Factory for one pooled backend instance. The returned instance owns
@@ -668,10 +677,13 @@ export const makeBackendInstance = Effect.fn("makeBackendInstance")(function* (
 
               if (isCurrentRun) {
                 if (Option.isSome(pid)) {
-                  yield* backendOutputLog.writeSessionBoundary({
-                    phase: "END",
-                    details: `pid=${pid.value} ${reason}`,
-                  });
+                  if (nextState.desiredRunning) {
+                    yield* backendOutputLog.persistFailure({
+                      details: `pid=${pid.value} ${reason}`,
+                    });
+                  } else {
+                    yield* backendOutputLog.discardSession;
+                  }
                 }
                 yield* spec.onShutdown?.() ?? Effect.void;
               }
@@ -692,8 +704,7 @@ export const makeBackendInstance = Effect.fn("makeBackendInstance")(function* (
               ...run,
               pid: Option.some(pid),
             }));
-            yield* backendOutputLog.writeSessionBoundary({
-              phase: "START",
+            yield* backendOutputLog.beginSession({
               details: `pid=${pid} port=${config.value.bootstrap.port} cwd=${config.value.cwd}`,
             });
           }),
@@ -719,10 +730,16 @@ export const makeBackendInstance = Effect.fn("makeBackendInstance")(function* (
 
             yield* spec.onReady?.(config.value.httpBaseUrl) ?? Effect.void;
           }),
-          onReadinessFailure: (error) =>
-            logInstanceWarning("backend readiness check failed during bootstrap", {
-              error: error.message,
-            }),
+          onReadinessFailure: Effect.fn("desktop.backendInstance.onReadinessFailure")(
+            function* (error) {
+              yield* logInstanceWarning("backend readiness check failed during bootstrap", {
+                error: error.message,
+              });
+              yield* backendOutputLog.persistFailure({
+                details: error.message,
+              });
+            },
+          ),
           onOutput: (streamName, chunk) => backendOutputLog.writeOutputChunk(streamName, chunk),
         }).pipe(
           Effect.provideService(ChildProcessSpawner.ChildProcessSpawner, spawner),
@@ -841,7 +858,7 @@ export const makeBackendInstance = Effect.fn("makeBackendInstance")(function* (
     });
     yield* Option.match(active, {
       onNone: () => Effect.void,
-      onSome: (run) => closeRun(run, options),
+      onSome: (run) => closeRun(run, options).pipe(Effect.andThen(backendOutputLog.discardSession)),
     });
   });
 